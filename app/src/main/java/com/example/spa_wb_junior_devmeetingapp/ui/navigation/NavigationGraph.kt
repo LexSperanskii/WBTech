36a2a261e0d288698dbd3acf289c2d9b14e3c0b9
Aleksansdr <sashaspj@gmail.com>
2024-08-03 14:29:31 +0100

    change navigation

--- app/src/main/java/com/example/spa_wb_junior_devmeetingapp/ui/navigation/NavigationGraph.kt
+++ app/src/main/java/com/example/spa_wb_junior_devmeetingapp/ui/navigation/NavigationGraph.kt
@@ -2,6 +2,7 @@ package com.example.spa_wb_junior_devmeetingapp.ui.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -58,151 +59,168 @@ internal fun NavHost(
         composable(route = DeveloperDestination.route) {
             DeveloperScreen()
         }
-        navigation(
-            startDestination = AuthenticationDestination.route,
-            route = "registration_tab"
-        ){
-            composable(route = AuthenticationDestination.route) {
-                AuthenticationScreen(
-                    navigateToVerificationScreen = {
-                        navController.navigate(VerificationDestination.route)
-                    },
-                    onClickNavigateBack = { navController.popBackStack() }
-                )
-            }
-            composable(route = VerificationDestination.route) {
-                VerificationScreen(
-                    onClickNavigateBack = { navController.popBackStack() } ,
-                    navigateToRegistrationProfile = {
-                        navController.navigate(RegistrationProfileDestination.route)
-                    }
-                )
-            }
-            composable(route = RegistrationProfileDestination.route) {
-                RegistrationProfileScreen(
-                    onClickNavigateBack = { navController.popBackStack() },
-                    navigateToEventsAllScreen = {
-                        navController.navigate(BottomNavItem.Events.route) {
-                            popUpTo("registration_tab") {
-                                inclusive = true
-                            }
+        registrationNav(navController = navController)
+        eventsNav(navController = navController)
+        communitiesNav(navController = navController)
+        menuNav(navController = navController)
+    }
+}
+
+internal fun NavGraphBuilder.registrationNav(navController: NavHostController) {
+    navigation(
+        startDestination = AuthenticationDestination.route,
+        route = "registration_tab"
+    ) {
+        composable(route = AuthenticationDestination.route) {
+            AuthenticationScreen(
+                navigateToVerificationScreen = {
+                    navController.navigate(VerificationDestination.route)
+                },
+                onClickNavigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = VerificationDestination.route) {
+            VerificationScreen(
+                onClickNavigateBack = { navController.popBackStack() },
+                navigateToRegistrationProfile = {
+                    navController.navigate(RegistrationProfileDestination.route)
+                }
+            )
+        }
+        composable(route = RegistrationProfileDestination.route) {
+            RegistrationProfileScreen(
+                onClickNavigateBack = { navController.popBackStack() },
+                navigateToEventsAllScreen = {
+                    navController.navigate(BottomNavItem.Events.route) {
+                        popUpTo("registration_tab") {
+                            inclusive = true
                         }
                     }
-                )
-            }
+                }
+            )
         }
-        navigation(
-            startDestination = EventsAllDestination.route,
-            route = BottomNavItem.Events.route
-        ){
-            composable(route = EventsAllDestination.route) {
-                EventsAllScreen(
-                    navController = navController,
-                    navigateToEventDetailItem = {
-                        navController.navigate("${BottomNavItem.Events.route}/${EventDetailsDestination.route}/${it}")
-                    },
-                    navigateToDeveloperScreen = {navController.navigate(DeveloperDestination.route)}
-                )
-            }
-            composable(
-                route = "${BottomNavItem.Events.route}/${EventDetailsDestination.routeWithArgs}",
-                arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
-                    type = NavType.IntType
-                })
-            ) {
-                EventDetailsScreen(
-                    navController = navController,
-                    navigateToFullScreenMap = {
-                        navController.navigate("${BottomNavItem.Events.route}/${MapDestination.route}")
-                    }
-                )
-            }
-            composable(
-                route = "${BottomNavItem.Events.route}/${MapDestination.route}")
-            {
-                FullScreenMapScreen(navController = navController)
-            }
+    }
+}
+
+internal fun NavGraphBuilder.eventsNav(navController: NavHostController) {
+    navigation(
+        startDestination = EventsAllDestination.route,
+        route = BottomNavItem.Events.route
+    ) {
+        composable(route = EventsAllDestination.route) {
+            EventsAllScreen(
+                navController = navController,
+                navigateToEventDetailItem = {
+                    navController.navigate("${BottomNavItem.Events.route}/${EventDetailsDestination.route}/${it}")
+                },
+                navigateToDeveloperScreen = { navController.navigate(DeveloperDestination.route) }
+            )
         }
-        navigation(
-            startDestination = CommunitiesDestination.route,
-            route = BottomNavItem.Communities.route
-        ){
-            composable(route = CommunitiesDestination.route) {
-                CommunityScreen(
-                    navController = navController,
-                    navigateToCommunityDetailItem = {
-                        navController.navigate("${CommunityDetailsDestination.route}/${it}")
-                    }
-                )
-            }
-            composable(
-                route = CommunityDetailsDestination.routeWithArgs,
-                arguments = listOf(navArgument(CommunityDetailsDestination.itemIdArg) {
-                    type = NavType.IntType
-                })
-            ) {
-                CommunityDetailsScreen(
-                    navController = navController,
-                    navigateToEventDetailItem = {
-                        navController.navigate("${BottomNavItem.Communities.route}/${EventDetailsDestination.route}/${it}")
-                    }
-                )
-            }
-            composable(
-                route = "${BottomNavItem.Communities.route}/${EventDetailsDestination.routeWithArgs}",
-                arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
-                    type = NavType.IntType
-                })
-            ) {
-                EventDetailsScreen(
-                    navController = navController,
-                    navigateToFullScreenMap = {
-                        navController.navigate("${BottomNavItem.Communities.route}/${MapDestination.route}")
-                    }
-                )
-            }
-            composable(route = "${BottomNavItem.Communities.route}/${MapDestination.route}") {
-                FullScreenMapScreen(navController = navController)
-            }
+        composable(
+            route = "${BottomNavItem.Events.route}/${EventDetailsDestination.routeWithArgs}",
+            arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EventDetailsScreen(
+                navController = navController,
+                navigateToFullScreenMap = {
+                    navController.navigate("${BottomNavItem.Events.route}/${MapDestination.route}")
+                }
+            )
         }
-        navigation(
-            startDestination = MenuDestination.route,
-            route = BottomNavItem.Menu.route
-        ){
-            composable(route = MenuDestination.route) {
-                MenuScreen(
-                    navController = navController,
-                    navigateToProfile = {navController.navigate(ProfileDestination.route)},
-                    navigateToUserEvents = {navController.navigate(EventsUserDestination.route)}
-                )
-            }
-            composable(route = ProfileDestination.route) {
-                ProfileScreen(navController = navController)
-            }
-            composable(route = EventsUserDestination.route) {
-                EventsUserScreen(
-                    navController = navController,
-                    navigateToEventDetailItem = {
-                        navController.navigate("${BottomNavItem.Menu.route}/${EventDetailsDestination.route}/${it}")
-                    }
-                )
-            }
-            composable(
-                route = "${BottomNavItem.Menu.route}/${EventDetailsDestination.routeWithArgs}",
-                arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
-                    type = NavType.IntType
-                })
-            ) {
-                EventDetailsScreen(
-                    navController = navController,
-                    navigateToFullScreenMap = {
-                        navController.navigate("${BottomNavItem.Menu.route}/${MapDestination.route}")
-                    }
-                )
-            }
-            composable(route = "${BottomNavItem.Menu.route}/${MapDestination.route}") {
-                FullScreenMapScreen(navController = navController)
-            }
+        composable(
+            route = "${BottomNavItem.Events.route}/${MapDestination.route}"
+        )
+        {
+            FullScreenMapScreen(navController = navController)
+        }
+    }
+}
+
+internal fun NavGraphBuilder.communitiesNav(navController: NavHostController) {
+    navigation(
+        startDestination = CommunitiesDestination.route,
+        route = BottomNavItem.Communities.route
+    ) {
+        composable(route = CommunitiesDestination.route) {
+            CommunityScreen(
+                navController = navController,
+                navigateToCommunityDetailItem = {
+                    navController.navigate("${CommunityDetailsDestination.route}/${it}")
+                }
+            )
+        }
+        composable(
+            route = CommunityDetailsDestination.routeWithArgs,
+            arguments = listOf(navArgument(CommunityDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            CommunityDetailsScreen(
+                navController = navController,
+                navigateToEventDetailItem = {
+                    navController.navigate("${BottomNavItem.Communities.route}/${EventDetailsDestination.route}/${it}")
+                }
+            )
+        }
+        composable(
+            route = "${BottomNavItem.Communities.route}/${EventDetailsDestination.routeWithArgs}",
+            arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EventDetailsScreen(
+                navController = navController,
+                navigateToFullScreenMap = {
+                    navController.navigate("${BottomNavItem.Communities.route}/${MapDestination.route}")
+                }
+            )
+        }
+        composable(route = "${BottomNavItem.Communities.route}/${MapDestination.route}") {
+            FullScreenMapScreen(navController = navController)
+        }
+    }
+}
+
+internal fun NavGraphBuilder.menuNav(navController: NavHostController) {
+    navigation(
+        startDestination = MenuDestination.route,
+        route = BottomNavItem.Menu.route
+    ) {
+        composable(route = MenuDestination.route) {
+            MenuScreen(
+                navController = navController,
+                navigateToProfile = { navController.navigate(ProfileDestination.route) },
+                navigateToUserEvents = { navController.navigate(EventsUserDestination.route) }
+            )
+        }
+        composable(route = ProfileDestination.route) {
+            ProfileScreen(navController = navController)
+        }
+        composable(route = EventsUserDestination.route) {
+            EventsUserScreen(
+                navController = navController,
+                navigateToEventDetailItem = {
+                    navController.navigate("${BottomNavItem.Menu.route}/${EventDetailsDestination.route}/${it}")
+                }
+            )
+        }
+        composable(
+            route = "${BottomNavItem.Menu.route}/${EventDetailsDestination.routeWithArgs}",
+            arguments = listOf(navArgument(EventDetailsDestination.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            EventDetailsScreen(
+                navController = navController,
+                navigateToFullScreenMap = {
+                    navController.navigate("${BottomNavItem.Menu.route}/${MapDestination.route}")
+                }
+            )
+        }
+        composable(route = "${BottomNavItem.Menu.route}/${MapDestination.route}") {
+            FullScreenMapScreen(navController = navController)
         }
     }
 }
