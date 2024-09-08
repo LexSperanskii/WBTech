19feb4e8e1e3d030e864e46b30d232e40d686f56
Aleksansdr <sashaspj@gmail.com>
2024-09-08 05:36:49 +0300

    add profileInterestsScreen

---
+++ ui_v2/src/main/java/com/example/ui_v2/ui/screens/userScreen/profileInterests/ProfileInterestsScreen.kt
@@ -0,0 +1,116 @@
+package com.example.ui_v2.ui.screens.userScreen.profileInterests
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.example.ui_v2.R
+import com.example.ui_v2.navigation.NavigationDestination
+import com.example.ui_v2.ui.components.ButtonWithStatus
+import com.example.ui_v2.ui.components.TagBig
+import com.example.ui_v2.ui.theme.DevMeetingAppTheme
+import com.example.ui_v2.ui.utils.ButtonStatus
+import org.koin.androidx.compose.koinViewModel
+
+internal object ProfileInterestsScreenDestination : NavigationDestination {
+    override val route = "profile_interests_screen"
+}
+
+@Composable
+internal fun ProfileInterestsScreen(
+    onSafeButtonClick: () -> Unit,
+    viewModel: ProfileInterestsScreenViewModel = koinViewModel(),
+) {
+    val profileInterestsScreenUiState by viewModel.getProfileInterestsScreenUiStateFlow()
+        .collectAsStateWithLifecycle()
+
+    Scaffold { innerPadding ->
+        ProfileInterestsScreenBody(
+            listOfTags = profileInterestsScreenUiState.listOfTags,
+            listOfChosenTags = profileInterestsScreenUiState.listOfChosenTags,
+            onTagClick = {
+                viewModel.onTagClick(it)
+            },
+            isButtonEnabled = profileInterestsScreenUiState.isButtonEnabled,
+            onButtonClick = onSafeButtonClick,
+            buttonStatus = profileInterestsScreenUiState.buttonStatus,
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+internal fun ProfileInterestsScreenBody(
+    listOfTags: List<String>,
+    listOfChosenTags: List<String>,
+    onTagClick: (String) -> Unit,
+    isButtonEnabled: Boolean,
+    onButtonClick: () -> Unit,
+    buttonStatus: ButtonStatus,
+    modifier: Modifier = Modifier,
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    Column(
+        modifier = modifier.padding(
+            start = 16.dp,
+            end = 16.dp,
+            top = 20.dp,
+            bottom = 28.dp
+        )
+    ) {
+        Text(
+            text = stringResource(id = R.string.interests),
+            color = DevMeetingAppTheme.colors.black,
+            style = DevMeetingAppTheme.typography.customH1,
+            modifier = Modifier
+                .padding(bottom = 12.dp)
+        )
+        Text(
+            text = stringResource(id = R.string.interests_description),
+            color = DevMeetingAppTheme.colors.black,
+            style = DevMeetingAppTheme.typography.subheading1,
+            modifier = Modifier.padding(bottom = 24.dp)
+        )
+        LazyColumn(
+            modifier = Modifier.weight(1f)
+        ) {
+            item {
+                FlowRow(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+                    listOfTags.forEach {
+                        TagBig(
+                            tagText = it,
+                            onTagClick = { onTagClick(it) },
+                            isClicked = listOfChosenTags.contains(it)
+                        )
+                    }
+                }
+            }
+        }
+        ButtonWithStatus(
+            notPressedText = stringResource(id = R.string.safe),
+            onClick = onButtonClick,
+            buttonStatus = buttonStatus,
+            isButtonEnabled = isButtonEnabled,
+            modifier = Modifier
+        )
+    }
+}
---
+++ ui_v2/src/main/java/com/example/ui_v2/ui/screens/userScreen/profileInterests/ProfileInterestsScreenViewModel.kt
@@ -0,0 +1,55 @@
+package com.example.ui_v2.ui.screens.userScreen.profileInterests
+
+import androidx.lifecycle.ViewModel
+import com.example.ui_v2.ui.utils.ButtonStatus
+import com.example.ui_v2.ui.utils.NewUIMockData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+internal data class ProfileInterestsScreenUiState(
+    val listOfTags: List<String> = listOf(),
+    val listOfChosenTags: List<String> = listOf(),
+    val buttonStatus: ButtonStatus = ButtonStatus.Active,
+) {
+    val isButtonEnabled: Boolean
+        get() = listOfChosenTags.isNotEmpty()
+}
+
+internal class ProfileInterestsScreenViewModel(
+    private val mock: NewUIMockData,
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ProfileInterestsScreenUiState())
+    private val uiState: StateFlow<ProfileInterestsScreenUiState> = _uiState.asStateFlow()
+
+    init {
+        _uiState.update {
+            it.copy(
+                listOfTags = mock.getListOfTags()
+            )
+        }
+    }
+
+    fun getProfileInterestsScreenUiStateFlow(): StateFlow<ProfileInterestsScreenUiState> = uiState
+
+    fun onTagClick(tag: String) {
+        _uiState.update { state ->
+            state.copy(
+                listOfChosenTags = when (state.listOfChosenTags.contains(tag)) {
+                    true -> {
+                        mock.removeFromMyChosenTags(tag)
+                        state.listOfChosenTags.toMutableList().apply { remove(tag) }
+                    }
+
+                    else -> {
+                        mock.addToMyChosenTags(tag)
+                        state.listOfChosenTags.toMutableList().apply { add(tag) }
+                    }
+                }
+            )
+        }
+    }
+
+}
